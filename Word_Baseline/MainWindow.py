153ea93184049515833f13e057082eed82ff93fe
A_X <78434551+Alex-XIA1@users.noreply.github.com>
2023-07-06 15:49:37 +0200

    Add files via upload

--- Word_Baseline/MainWindow.py
+++ Word_Baseline/MainWindow.py
@@ -28,6 +28,10 @@ class CustomTextEdit(QTextEdit):
             self.handle_redo()
         elif event.key() == Qt.Key_C and modifiers == Qt.ControlModifier:
             self.handle_copy()
+        elif event.key() == Qt.Key_U and modifiers == Qt.ControlModifier:
+            self.handle_underline()
+        elif event.key() == Qt.Key_B and modifiers == Qt.ControlModifier:
+            self.handle_bold()
         elif event.key() == Qt.Key_Left:
             self.handle_move_cursor_left()
         elif event.key() == Qt.Key_Right:
@@ -37,6 +41,8 @@ class CustomTextEdit(QTextEdit):
         super().keyPressEvent(event)
 
     def handle_copy(self):
+        # toPlainText() recupere le texte
+        print(self.toPlainText())
         print("J'ai copié")
 
     def handle_backspace(self):
@@ -62,21 +68,91 @@ class CustomTextEdit(QTextEdit):
 
     def handle_move_cursor_right(self):
         print("je vais à droite")
+    
 
 class MainWindow(QMainWindow):
-
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Word Like app")
 
-        self.editor = CustomTextEdit()
-
+        self.text_edit = CustomTextEdit(self)
 
         self.setFixedSize(QSize(400, 300))
 
-        # Set the central widget of the Window.
-        self.setCentralWidget(self.editor)
+        # Définit le widget central de la fenêtre.
+        self.setCentralWidget(self.text_edit)
+
+        search_action = QAction(QIcon(), "Search", self)
+        search_action.setShortcut("Ctrl+F")
+        search_action.triggered.connect(self.search)
+
+        replace_action = QAction(QIcon(), "Replace", self)
+        replace_action.setShortcut("Ctrl+R")
+        replace_action.triggered.connect(self.replace)
+
+        bold_action = QAction(QIcon(), "Bold", self)
+        bold_action.setShortcut("Ctrl+B")
+        bold_action.setCheckable(True)
+        bold_action.toggled.connect(self.toggle_bold)
+
+        underline_action = QAction(QIcon(), "Underline", self)
+        underline_action.setShortcut("Ctrl+U")
+        underline_action.setCheckable(True)
+        underline_action.toggled.connect(self.toggle_underline)
+
+
+        menu = self.menuBar()
+        edit_menu = menu.addMenu("&Edit")
+        edit_menu.addAction(search_action)
+        edit_menu.addAction(replace_action)
+        edit_menu.addSeparator()
+        edit_menu.addAction(bold_action)
+        edit_menu.addAction(underline_action)
+
+    def search(self):
+        # Demande à l'utilisateur d'entrer le texte à rechercher
+        search_text, ok = QInputDialog.getText(self, "seach", "Enter text to search:")
+        if ok:
+            # Recherche la première occurrence du texte recherché dans le widget d'édition de texte
+            cursor = self.text_edit.document().find(search_text)
+            if not cursor.isNull():
+                # Positionne le curseur de texte sur la position trouvée
+                self.text_edit.setTextCursor(cursor)
+                # Assure que le curseur est visible dans le widget d'édition de texte
+                self.text_edit.ensureCursorVisible()
+            else:
+                # Informe l'utilisateur que le texte n'a pas été trouvé
+                QMessageBox.information(self, "Search", "Text not found.")
+
+    def replace(self):
+        # Demande à l'utilisateur d'entrer le texte à remplacer
+        search_text, ok = QInputDialog.getText(self, "Replace", "Enter text to replace:")
+        if ok:
+            # Demande à l'utilisateur d'entrer le texte de remplacement
+            replace_text, ok = QInputDialog.getText(self, "Replace", "Enter replacement text:")
+            if ok:
+                document = self.text_edit.document()
+                # Recherche la première occurrence du texte recherché dans le widget d'édition de texte
+                cursor = document.find(search_text)
+                while not cursor.isNull():
+                    # Remplace le texte par le texte de remplacement
+                    cursor.insertText(replace_text)
+                    # Recherche l'occurrence suivante du texte recherché
+                    cursor = document.find(search_text, cursor)
+
+    def toggle_bold(self, boolgras):
+        #Transforme le texte en gras (built in dans pyqt)
+        font = self.text_edit.currentFont()
+        font.setBold(boolgras)
+        self.text_edit.setCurrentFont(font)
+
+    def toggle_underline(self, boolsouligner):
+        #Souligne le texte selectionnee 
+        font = self.text_edit.currentFont()
+        font.setUnderline(boolsouligner)
+        self.text_edit.setCurrentFont(font)
+
 
     
 
