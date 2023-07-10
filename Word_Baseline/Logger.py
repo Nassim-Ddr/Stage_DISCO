52ed5554c2653ab85288b44e11eaab2dc2bfedd9
A_X <78434551+Alex-XIA1@users.noreply.github.com>
2023-07-10 14:19:59 +0200

    Add files via upload

--- Word_Baseline/Logger.py
+++ Word_Baseline/Logger.py
@@ -19,12 +19,20 @@ def bag_of_words(text):
     return bag_of_word
 
 
+def get_dict():
+    my_data = np.genfromtxt('data\word-freq-top5000.csv', delimiter=',', skip_header=True)[:,2]
+    print(my_data)
+
+def to_vectorize(text):
+    B = bag_of_words(text)
+    
+
+
+
 
 
 
 if __name__ == "__main__":
     testText = "You can train this autoencoder on the Olivetti faces dataset using stochastic gradient descent (SGD) or any other optimization algorithm of your choice. The goal of training is to minimize the reconstruction loss (BCELoss) between the input image and its reconstructed version. Once trained, you can use the encoder to extract features from the Olivetti faces dataset, and the decoder to reconstruct the original images from the encoded features."
     bow = bag_of_words(testText)
-    # Print the bag of words representation
-    for word, count in bow.items():
-        print(f"{word}: {count}")
+    get_dict()
