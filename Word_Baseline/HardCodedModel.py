7a49d6054559619596501302b341d5d51965607d
A_X <78434551+Alex-XIA1@users.noreply.github.com>
2023-07-18 10:12:27 +0200

    hardcoded rules done ???????

--- Word_Baseline/HardCodedModel.py
+++ Word_Baseline/HardCodedModel.py
@@ -14,27 +14,55 @@ class HardCodedModel():
     def predict(self, inputData):
         inputData, label = inputData[:len(inputData)-1], inputData[-1]
         past, present = inputData[:5], inputData[5:]
-        cursorPos, line1, column1, line2, column2 = past
-        presentPos, presLine1, presColumn1, presLine2, presColumn2 = present
+        oldPos, oldline1, oldcolumn1, oldline2, oldcolumn2 = past
+        newPos, newLine1, newColumn1, newLine2, newColumn2 = present
 
-        # On etait plus loin dans le document
-        if cursorPos < presentPos and presentPos-cursorPos > 2 :
-            # dans le cas où la selection se passe sur la meme ligne
-            if line1 == presLine1 :
-                if line2 == presLine2 :
-                    if column1 == presColumn1 :
-                        if column2 < presColumn2 :
-                            # l'utilisateur décide de deselectionner un element
+
+
+        # On va plus loin dans le document
+        if oldPos < newPos and newPos-oldPos > 2 :
+            # dans le cas où la selection se passe sur la meme ligne (si la ligne est differente, l'utilisateur se deplace vers la droite)
+            if oldline1 == newLine1 :
+                # si la colonne est differente ici, l'utilisateur ne fait que se deplacer (vers la droite)
+                if oldcolumn1 == newColumn1 :
+                    # La fin de la selection se trouve sur la meme ligne
+                    if oldline2 == newLine2 :
+                        if oldcolumn2 > newColumn2 :
+                            # l'utilisateur décide de selectionner un element (un mot ou groupe de lettres, comment differencier son intention ?)
                             print("CTRL + Shift + Left")
-                        elif column2 > presColumn2 :
-                            # l'utilisateur décide de selectionner des elements en plus sur une ligne
+                        elif oldcolumn2 < newColumn2 :
+                            # l'utilisateur décide de deselectionner des elements en plus sur une ligne
                             print("CTRL + Shift + Right")
                         else:
                             # L'utilisateur s'est déplacé
                             print("CTRL + Right")
-
-            
-
-        # c'est juste un deplacement vers la droite il n'y a rien à suggérer
+                    else :
+                        # Une selection sur la meme ligne, donc la fin est plus loin vers la droite dans ce cas
+                        print("CTRL + Shift + Right")
+                else:
+                    print("Deplacement droite (CTRL+Right pour se deplacer de mot en mot)")
+            else:
+                print("Deplacement droite (CTRL+Right pour se deplacer de mot en mot)")
+        elif oldPos == newPos :
+            print("rien ne s'est passe")
+        # La selection se fait de droite à gauche
         else :
-            print("Right")
+            if oldline2 == newLine2:
+                if oldcolumn2 == newColumn2:
+                    if oldline1 == newLine1:
+                        # old > new -> mouvement vers la gauche
+                        if oldcolumn1 > newColumn1:
+                            print("CTRL + Shift + Left")
+                        # old < new -> mouvement vers la droite
+                        elif oldcolumn1 < newColumn1:
+                            print("CTRL + Shift + Right")
+                        else:
+                            print("CTRL + Left")
+                    else :
+                        # si on passe a la ligne precedente, on sait que c'est une selection de droite a gauche
+                        print("CTRL + Shift + Left")
+                else:
+                    print("Deplacement gauche (CTRL+Left pour se deplacer de mot en mot)")
+            else :
+                print("Deplacement gauche (CTRL+Left pour se deplacer de mot en mot)")
+        
