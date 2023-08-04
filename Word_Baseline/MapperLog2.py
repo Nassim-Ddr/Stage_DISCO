d6538be138cd1aae953c0c780e3df897061c39a4
A_X <78434551+Alex-XIA1@users.noreply.github.com>
2023-08-04 16:02:16 +0200

    tentative of lemmarization

--- Word_Baseline/MapperLog2.py
+++ Word_Baseline/MapperLog2.py
@@ -3,6 +3,7 @@ import re
 import pandas as pd
 import csv
 from time import sleep
+import spacy
 
 from sklearn.feature_extraction.text import CountVectorizer
 
@@ -16,7 +17,12 @@ class MapperLog2():
         #my_data = pd.read_csv('data\word-freq-top5000.csv', delimiter=',', usecols=[1]).to_numpy()[:,0]
         my_data = pd.read_csv('data/NGSLWords.csv', delimiter=',', usecols=[0]).to_numpy()[:,0]
         toadd = "The sun rises, and the morning brings a new day. Birds chirp, filling the air with melody.\nI wake up, feel refreshed, and get ready for the day ahead. I put on a comfortable shirt and jeans,\nready for a walk in the nearby park. As I stroll through the park, I see many people enjoying the outdoors.\nSome jog, others walk their dog. A child plays on the playground, laughing and having fun. It's a pleasant sight.\nIn the park, there is a tall tree providing shade from the sun. I find a bench under a tree and sit down to read a book.\nThe story is captivating, and time passes quickly. I lose myself in the pages, engrossed in the tale.\nAfter a while, I decide to explore more of the park. There is a pond with a duck swimming peacefully.\nI watch it for a while before continuing on my way. A group of friends has a picnic nearby,\nsharing food and laughter. As I walk, I notice a beautiful flower in various colors - red, blue, yellow, and white.\nThe fragrance of the flower fills the air,\nmaking the stroll even more delightful. I take a moment to admire the beauty of nature.\nAfter a pleasant walk, I head to a café for lunch. The café is cozy, and the menu offers a variety of dishes.\nI order a sandwich and a refreshing lemonade. The food is delicious, and I enjoy the peaceful atmosphere of the café.\nIn the afternoon, I meet a friend at the library. We browse through books, discussing our favorite author and genre.\nThe library is a treasure trove of knowledge, and we leave with a few books to read later.\nLater in the evening, I attend a music concert in the park. The band plays lively tunes,\nand the crowd claps along. The atmosphere is festive, and everyone enjoys the performance.\nAs the sun sets, the sky changes colors, displaying shades of orange and pink. It's a beautiful sight.\nIn conclusion, spending time outdoors and appreciating the simple pleasure of life can bring joy and fulfillment.\nThe NGSL provides a wide range of words to express the experience and emotion we encounter every day."
-        toadd = np.array([toadd])
+        # toadd = np.array([toadd])
+
+        # Load the language model
+        self.nlp = spacy.load("en_core_web_sm")
+        #doc = self.nlp(toadd)
+        #lemmtoadd = [token.lemma_ for token in doc]
         my_data = np.hstack((my_data,toadd))
         #my_data = "Darkness. Just darkness. Darkness not visible. The absence of light. A vacuum I can’t describe. An . . . emptiness.\n\nDarkness in which I can see nothing. Darkness that terrifies me, suffocates me, crushes me. Darkness forced on me whether I like it or not, whether it is daylight or nighttime outside, in which I am expected to sleep. Darkness created by window coverings that cut off light and fresh air, the windows further curtained to prevent stray outside light from entering my room. Darkness.\n\nIn the darkness all I can hear is my clock. And my own heartbeat. And my breathing. At least I am alive. Or am I? It is hard to be sure in the darkness. Darkness, and voices. The voices of my parents, though I am alone, far from them: “Child do this . . . Child do that . . . Child don’t . . . Child why can’t you . . . Child stop . . . Child you must . . . Child, child, child.” Never, ever my name.\n\nLying there, I feel as if I am being forced into a pit, a hole in the ground—being buried, hidden, put away. As if I am disposable. As if my very existence is being denied. As if I must not be seen or heard. As if my birth is a dirty secret, an evil act of mine that must be obliterated without trace. As if I am an object of . . . shame. Why? What could I, a mere child, have done that would cause such a reaction in others, in my father and mother—the man and woman who created me, guardians and enforcers of my darkness?\n\nI am their only child. I think I know why: they never wanted me. I was an accident for them, a mistake they will be careful never to repeat."
         self.vectorizer.fit_transform(my_data)
@@ -49,9 +55,13 @@ class MapperLog2():
         # format :
         text,word_count,charlen, cursorPos, cursorEnd, selStart,selEnd = state
 
+        #doc = self.nlp(text)
+        #text = [token.lemma_ for token in doc]
+
         line2, column2 = cursorEnd
 
         treated = self.vectorizer.transform([text]).toarray()[0]
+        #treated = self.vectorizer.transform(text).toarray()[0]
         word_count -= np.sum(treated)
         treated = np.hstack([treated,[word_count],[charlen]])
         treatedPos = np.array([cursorPos,line2,column2,selStart,selEnd]).astype(int)
